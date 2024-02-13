4840b83c7ccd0fbae53aec0b9093269572d990f2
Sfnadeem <nadeem.malik@sourcefuse.com>
2024-02-13 15:45:20 +0530

    dotnet version changed

--- Startup.cs
+++ Startup.cs
@@ -10,7 +10,7 @@ namespace HelloWeb
         {
             app.Run(context =>
             {
-                return context.Response.WriteAsync("Hello World!, New packer image");
+                return context.Response.WriteAsync("Hello World!, My Packer AMI");
             });
         }
     }
